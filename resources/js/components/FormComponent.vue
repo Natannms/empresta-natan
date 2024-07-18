519fa7eb63e9aa35ab65198e14cfc052e84d9ca4
Natannms <agnusnat.nms@gmail.com>
2024-07-18 16:35:50 -0300

    Feat (Tailwidcss): Adiciona tailwind css via cdn para estilização

---
+++ resources/js/components/FormComponent.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.bg-warning {
+    background-color: yellow !important;
+}
+.text-center {
+    text-align: center;
+}
+</style>
